c694b2969b56cf40099c13f88305086a5d1832f5
Gorka <gorka.molero@gmail.com>
2020-03-02 16:16:54 +0100

    Usuarios

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useFirestore, useFirestoreDocData } from 'reactfire'
+import { useUser, useFirestore, useFirestoreDocData } from 'reactfire'
 
 export function useConfig() {
   const currentPromoRef = useFirestore().doc('config/current')
@@ -13,11 +13,25 @@ export function useConfig() {
   const docsRef = useFirestore().doc('config/docs')
   const docs = useFirestoreDocData(docsRef)
 
+  const candidatesRef = useFirestore().doc('config/candidates')
+  const candidates = useFirestoreDocData(docsRef)
+
   const config = {
     currentPromo,
     ...info,
-    docs
+    docs,
+    candidates
   }
 
   return config
+}
+
+export function useFullUser() {
+  const user = useUser()
+  console.log('UYSER', user.uid)
+  const userDetailsRef = useFirestore().collection('candidates').doc(user.uid)
+  const profile = useFirestoreDocData(userDetailsRef)
+  console.log('Profile', profile)
+  user.profile = profile
+  return user
 }
